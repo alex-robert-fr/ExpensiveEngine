6307d2833ff80bb854752b597959196ae2916715
Alex Robert <x7c00@alex.home>
2023-10-19 20:36:15 +0200

    FEAT: Add index buffer

--- src/main.c
+++ src/main.c
@@ -26,9 +26,18 @@ int	main(void) {
 
 	// Initialisation position vertices
 	GLfloat vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+   	-0.5f, -0.5f * sqrtf(3) / 3, 0.0f,
+     0.5f, -0.5f * sqrtf(3) / 3, 0.0f,
+     0.0f,  0.5f * sqrtf(3) * 2 / 3, 0.0f,
+		 -0.5f / 2, 0.5f * sqrtf(3) / 6, 0.0f,
+		 0.5 / 2, 0.5f * sqrtf(3) / 6, 0.0f,
+		 0.0f, -0.5f * sqrtf(3) / 3, 0.0f
+	};
+
+	GLuint indices[] = {
+		0, 3, 5,
+		3, 2, 4,
+		5, 4, 1
 	};
 
 	// Create window
@@ -63,21 +72,26 @@ int	main(void) {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragementShader);
 
-	GLuint	VAO, VBO;
+	GLuint	VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -89,7 +103,7 @@ int	main(void) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -97,6 +111,7 @@ int	main(void) {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
